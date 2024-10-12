572ffcc54871bddd66f13344bcaf7b0c634d32d0
Mark Joseph Garcia <mjgarcia0301@gmail.com>
2024-10-13 01:22:21 +0800

    initial commit

---
+++ lanternusermanagement/lanternusermanagement/backend/src/main/java/com/callmax/lantern/repository/UsersRepo.java
@@ -0,0 +1,12 @@
+package com.callmax.lantern.repository;
+
+
+import com.callmax.lantern.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
